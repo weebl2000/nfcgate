e5205b035f04f64b6248c0a686d7a9fadd207f4e
Alexandros Roussos <thealexroussos@gmail.com>
2024-11-05 00:27:59 +0100

    Switch from /proc/self/maps to dl_iterate_phdr
    Co-Authored-By: Steffen Klee <sklee@seemoo.tu-darmstadt.de>

--- nfcd/src/main/jni/src/helper/MapInfo.cpp
+++ nfcd/src/main/jni/src/helper/MapInfo.cpp
@@ -4,35 +4,24 @@
 #include <fstream>
 #include <regex>
 
+#include <link.h>
+
 bool MapInfo::create() {
-    std::ifstream maps("/proc/self/maps");
-    LOG_ASSERT_S(maps.is_open(), return false, "Error loading proc maps");
+    int rv = dl_iterate_phdr([] (struct dl_phdr_info *info, size_t, void *user_data) {
+        auto *instance = (MapInfo *)user_data;
 
-    // line has format "<rangeStart>-<rangeEnd> <perms> <ignored1> <ignored2> ...    <label>"
-    std::regex re(R"(^([^\s-]+)-([^\s]+)\s([\w-]+)\s.*?(?:\s\s([^\s].+))?$)");
-    for (std::string line; std::getline(maps, line); ) {
-        // match regex, ignore non-matching lines
-        std::smatch matches;
-        if (!std::regex_match(line, matches, re))
-            continue;
+        for (size_t i = 0; i < info->dlpi_phnum; i++) {
+            instance->mRanges.push_back(RangeData {
+                info->dlpi_addr + info->dlpi_phdr[i].p_vaddr,
+                info->dlpi_addr + info->dlpi_phdr[i].p_vaddr + info->dlpi_phdr[i].p_memsz,
+                static_cast<uint8_t>(info->dlpi_phdr[i].p_flags),
+                info->dlpi_name
+            });
+        }
 
-        RangeData result;
-        // parse start and end from hex
-        result.start = std::stoull(matches.str(1), nullptr, 16);
-        result.end = std::stoull(matches.str(2), nullptr, 16);
-        // copy label
-        result.label = matches.str(4);
-
-        // perms has format "rwxp" or "----"
-        auto perm = matches.str(3);
-        LOG_ASSERT_S(perm.size() == 4, return false, "Error reading map perms");
-        int read = perm[0] == 'r' ? 4 : 0;
-        int write = perm[1] == 'w' ? 2 : 0;
-        int execute = perm[2] == 'x' ? 1 : 0;
-        result.perms = read + write + execute;
-
-        mRanges.push_back(result);
-    }
+        return 0;
+    }, this);
+    LOG_ASSERT_S(rv == 0, return false, "Error iterating dl program header");
 
     return true;
 }
@@ -41,9 +30,7 @@ std::set<std::string> MapInfo::loadedLibraries() const {
     std::set<std::string> result;
 
     for (auto &range : mRanges) {
-        if (StringUtil::strEndsWith(range.label, ".so")) {
-            result.emplace(range.label);
-        }
+        result.emplace(range.label);
     }
 
     return result;
